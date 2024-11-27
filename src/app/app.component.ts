17df224a58936c7d18377e89fbc104525fe6151b
Eltioflipe3D <jaimefs2005@gmail.com>
2024-11-27 20:46:05 +0100

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from './dbz/pages/main-page.components';
 
 @Component({
   selector: 'app-root',
